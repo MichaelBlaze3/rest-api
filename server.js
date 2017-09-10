df559725f6edffd2f5718bd07250bd1bd9bba677
MichaelBlaze3 <blas.michael@icloud.com>
2017-09-10 09:03:09 -0700

    Added an array that contains a list of products

--- server.js
+++ server.js
@@ -2,10 +2,25 @@ const express = require('express');
 const bodyparser = require('body-parser');
 
 const app = express();
+
+var store = [
+    {
+        id:'12214wffeferf',
+        product: 'Milk',
+        price: 2.99
+    }
+];
+
+
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({
     extended: true
 }));
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 
 app.get('/', function (req, res) {
     res.send('hello world');
@@ -14,6 +29,13 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) {
     res.send('hello from post');
 });
+
+app.post('/add', function (req, res) {
+    console.log(req.query);
+    store.push(req.query);
+    res.send('hello from post' + JSON.stringify(store));
+});
+
 
 app.listen(3000, function(){
     console.log("Running on 3000");
